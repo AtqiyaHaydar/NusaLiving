b63896fd3078fc90d65748caf6010266181c0114
AtqiyaHaydar <ashford1524@gmail.com>
2024-07-27 16:24:03 +0700

    feat: add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Github } from 'lucide-react'
 
 const Footer = () => {
   return (
     <footer className='w-full mt-24'>
-      <div className='wrapper w-full text-center'>
-        Footer
+      <div className='wrapper w-full text-sm md:text-[16px] text-center py-2 flex items-center justify-center gap-2'>
+        @AtqiyaHaydar <span className='hidden md:block'>|</span> <br className='md:hidden'></br> 🌱 Seleksi Calon Asisten Lab RPL - OHL 2024
       </div>
     </footer>
   )
